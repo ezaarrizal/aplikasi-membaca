629b18db46078714d95b6ba94d6e3ed77c38e2b0
ezaarrizal <139563543+ezaarrizal@users.noreply.github.com>
2025-07-01 12:28:23 +0700

    fix url vol 2

--- config/cors.php
+++ config/cors.php
@@ -9,6 +9,7 @@ return [
     'allowed_origins' => [
         'https://charming-haupia-7f4b25.netlify.app',
         'https://statuesque-heliotrope-0b87ef.netlify.app',
+        'https://ephemeral-rugelach-eec8d2.netlify.app',
     ],
 
     'allowed_origins_patterns' => [],
